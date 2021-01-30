86841ae6fc4a0975d8ef23396eb48160a9796205
Hemasree Darisiguntla <sreed2503@gmail.com>
2021-01-30 13:19:36 -0500

    Added test to validate status for playlist without name

--- src/main/java/com/musicshare/playlistapi/controller/PlayListController.java
+++ src/main/java/com/musicshare/playlistapi/controller/PlayListController.java
@@ -2,6 +2,7 @@ package com.musicshare.playlistapi.controller;
 
 import com.musicshare.playlistapi.entity.PlayList;
 import com.musicshare.playlistapi.entity.Song;
+import com.musicshare.playlistapi.exception.NameRequiredException;
 import com.musicshare.playlistapi.service.PlayListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,9 +17,10 @@ public class PlayListController {
     @Autowired
     PlayListService playListService;
 
-    @PostMapping("/{name}")
+    @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
-    public PlayList createPlayListWithName(@PathVariable String name) {
+    public PlayList createPlayListWithName(@RequestParam String name) throws NameRequiredException {
+
 
         return playListService.createPlayListWithName(name);
     }
--- src/test/java/com/musicshare/playlistapi/controller/PlayListControllerTest.java
+++ src/test/java/com/musicshare/playlistapi/controller/PlayListControllerTest.java
@@ -1,10 +1,13 @@
 package com.musicshare.playlistapi.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.musicshare.playlistapi.entity.PlayList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.hasSize;
@@ -20,14 +23,26 @@ public class PlayListControllerTest {
     @Autowired
     MockMvc mockMvc;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     @Test
     public void test_createPlayListWithName() throws Exception {
 
-        mockMvc.perform(post("/api/v1/playlist/{name}", "playlist1"))
+        mockMvc.perform(post("/api/v1/playlist")
+                .param("name","playlist1"))
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.id").exists())
                 .andExpect(jsonPath("$.name").value("playlist1"))
                 .andExpect(jsonPath("$.songs", hasSize(0)));
 
+    }
+
+    @Test
+    public void test_createPlayListWithoutName() throws Exception {
+
+        mockMvc.perform(post("/api/v1/playlist")
+                .param("name",""))
+                .andExpect(status().isBadRequest());
     }
 }
